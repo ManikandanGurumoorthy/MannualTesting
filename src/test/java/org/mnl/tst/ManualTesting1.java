836895e3a50f76f02fb4b882abc93f3c540fca93
ManikandanGurumoorthy <manikandangurumoorthy32@yahoo.com>
2022-09-18 05:11:23 +0530

    manualtesting1

---
+++ src/test/java/org/mnl/tst/ManualTesting1.java
@@ -0,0 +1,20 @@
+package org.mnl.tst;
+
+public class ManualTesting1 {
+
+	private void m1() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void m2() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void m3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
